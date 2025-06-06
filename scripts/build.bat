ef9cabc56b67fd2a7d45bd00f5c4379aae274de3
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2025-06-06 11:24:41 +0200

    Saving work

---
+++ scripts/build.bat
@@ -0,0 +1,40 @@
+@echo off
+
+setlocal
+
+set /p VERSION=<VERSION
+
+if /I "%~1"=="" (
+    echo "Usage: build.bat [--briefcase|--nuitka]"
+    exit /b 1
+)
+
+set START_DIR=%CD%
+
+if /I "%~1"=="--briefcase" (
+
+    rmdir /s /q castoranalytics\build
+    cd castoranalytics
+    call briefcase create
+    call briefcase build
+
+) else if /I "%~1"=="--nuitka" (
+
+    rmdir /s /q castoranalytics\build-nuitka
+    cd castoranalytics\src
+    call nuitka ^
+        --standalone ^
+        --enable-plugin=pyside6 ^
+        --include-qt-plugins=all ^
+        --include-data-dir=castoranalytics/resources=castoranalytics/resources ^
+        --windows-console-mode=disable ^
+        --output-dir=..\build-nuitka ^
+        --output-filename=CastorAnalytics ^
+        app.py
+) else (
+    echo "Unknown option"
+)
+
+cd %START_DIR%
+
+endlocal
--- scripts/run.bat
+++ scripts/run.bat
@@ -4,19 +4,27 @@ setlocal
 
 set /p VERSION=<VERSION
 
+if /I "%~1"=="" (
+    echo "Usage: run.bat [--dev|--test|--exe|--exe-nuitka]"
+    exit /b 1
+)
+
 set START_DIR=%CD%
 
-if /I "%~1"=="--test" (
+if /I "%~1"=="--dev" (
+    cd castoranalytics
+    call briefcase dev
+) else if /I "%~1"=="--test" (
     cd castoranalytics
     call briefcase dev --test
 ) else if /I "%~1"=="--exe" (
     cd castoranalytics
     call briefcase run
-) else if /I "%~1"=="--nuitka" (
+) else if /I "%~1"=="--exe-nuitka" (
     cd castoranalytics\build-nuitka\app.dist
     call CastorAnalytics.exe
 ) else (
-    call briefcase dev
+    echo "Unknown option"
 )
 
 cd %START_DIR%
