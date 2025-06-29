ee52b0f68c2ad1700a290f001cb8dcbf7756fa80
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-06-29 22:22:47 +0200

    Simplified study class

--- castoranalytics/src/castoranalytics/core/data/study.py
+++ castoranalytics/src/castoranalytics/core/data/study.py
@@ -2,8 +2,6 @@ class Study:
     def __init__(self):
         self._id = None
         self._name = None
-        self._fields = None
-        self._optiongroups = None
 
     def id(self):
         return self._id
@@ -16,18 +14,6 @@ class Study:
     
     def set_name(self, name):
         self._name = name
-
-    def fields(self):
-        return self._fields
-    
-    def set_fields(self, fields):
-        self._fields = fields
-
-    def optiongroups(self):
-        return self._optiongroups
-    
-    def set_optiongroups(self, optiongroups):
-        self._optiongroups = optiongroups
 
     def __str__(self):
         return f'Study(id={self.id()}, name={self.name()})'
