9a7c465727f401993653e50899cdc02572fbc8ed
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-06-29 23:05:48 +0200

    Working on study table

---
+++ castoranalytics/src/castoranalytics/ui/components/tables/studysitetable.py
@@ -0,0 +1,10 @@
+from castoranalytics.ui.components.tables.table import Table
+
+
+class StudySiteTable(Table):
+    def __init__(self):
+        super(StudySiteTable, self).__init__()
+
+    def load_data(self):
+        # Use StudyLoader to load data
+        pass
---
+++ castoranalytics/src/castoranalytics/ui/components/tables/studytable.py
@@ -0,0 +1,10 @@
+from castoranalytics.ui.components.tables.table import Table
+
+
+class StudyTable(Table):
+    def __init__(self):
+        super(StudyTable, self).__init__()
+
+    def load_data(self):
+        # Use StudyLoader to load data
+        pass
